103f8bc925ffa707de93c1c2f92b756ab33b3d73
SirichandanaChennareddy <151725976+SirichandanaChennareddy@users.noreply.github.com>
2023-12-19 15:51:43 +0530

    Uploaded SQL Activities

---
+++ SQL/Activities/Activity_4_5.sql
@@ -0,0 +1,21 @@
+REM   Script: Activity4,5
+REM   This Script is for Activity4 and 5
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
+SELECT * FROM salesman;
+
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+
+SELECT * FROM salesman;
+
